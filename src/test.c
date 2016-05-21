5eb711ab393f51b2ea4aebcd9eb05ee0119a85a1
eitanhaim <eitanhaim@mail.tau.ac.il>
2016-05-21 22:37:27 +0300

    Add Csmith

---
+++ src/test.c
@@ -0,0 +1,14 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1257846327
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
